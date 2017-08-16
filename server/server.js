b21c7700ad0fffcd951f74a137d2b2cd71935121
Marcos Vinicius <marcosvsgouvea@gmail.com>
2017-08-16 14:46:13 -0300

    add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,23 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+      from: 'Vinicius',
+      text: 'aew manolex',
+      createdAt:123123
+  });
+
+  socket.emit('newEmail', {
+    from: 'marcosvsgouvea@gmail.com',
+    text: 'whats going on',
+    createAt: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
+
   socket.on('disconnect', () => {
     console.log('user was disconnected');
   });
